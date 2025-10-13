9b4a40938c0483e9b3e473ef5b3e46c6f3500292
Jihwan <wlghks9619@gmail.com>
2025-10-13 11:48:48 +0900

    초기 커밋.

---
+++ MFCBreadFactory/MFCBreadFactoryView.cpp
@@ -0,0 +1,84 @@
+﻿
+// MFCBreadFactoryView.cpp: CMFCBreadFactoryView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "MFCBreadFactory.h"
+#endif
+
+#include "MFCBreadFactoryDoc.h"
+#include "MFCBreadFactoryView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFCBreadFactoryView
+
+IMPLEMENT_DYNCREATE(CMFCBreadFactoryView, CFormView)
+
+BEGIN_MESSAGE_MAP(CMFCBreadFactoryView, CFormView)
+END_MESSAGE_MAP()
+
+// CMFCBreadFactoryView 생성/소멸
+
+CMFCBreadFactoryView::CMFCBreadFactoryView() noexcept
+	: CFormView(IDD_MFCBREADFACTORY_FORM)
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+
+}
+
+CMFCBreadFactoryView::~CMFCBreadFactoryView()
+{
+}
+
+void CMFCBreadFactoryView::DoDataExchange(CDataExchange* pDX)
+{
+	CFormView::DoDataExchange(pDX);
+}
+
+BOOL CMFCBreadFactoryView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: CREATESTRUCT cs를 수정하여 여기에서
+	//  Window 클래스 또는 스타일을 수정합니다.
+
+	return CFormView::PreCreateWindow(cs);
+}
+
+void CMFCBreadFactoryView::OnInitialUpdate()
+{
+	CFormView::OnInitialUpdate();
+	GetParentFrame()->RecalcLayout();
+	ResizeParentToFit();
+
+}
+
+
+// CMFCBreadFactoryView 진단
+
+#ifdef _DEBUG
+void CMFCBreadFactoryView::AssertValid() const
+{
+	CFormView::AssertValid();
+}
+
+void CMFCBreadFactoryView::Dump(CDumpContext& dc) const
+{
+	CFormView::Dump(dc);
+}
+
+CMFCBreadFactoryDoc* CMFCBreadFactoryView::GetDocument() const // 디버그되지 않은 버전은 인라인으로 지정됩니다.
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMFCBreadFactoryDoc)));
+	return (CMFCBreadFactoryDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMFCBreadFactoryView 메시지 처리기
---
+++ MFCBreadFactory/MFCBreadFactoryView.h
@@ -0,0 +1,52 @@
+﻿
+// MFCBreadFactoryView.h: CMFCBreadFactoryView 클래스의 인터페이스
+//
+
+#pragma once
+
+
+class CMFCBreadFactoryView : public CFormView
+{
+protected: // serialization에서만 만들어집니다.
+	CMFCBreadFactoryView() noexcept;
+	DECLARE_DYNCREATE(CMFCBreadFactoryView)
+
+public:
+#ifdef AFX_DESIGN_TIME
+	enum{ IDD = IDD_MFCBREADFACTORY_FORM };
+#endif
+
+// 특성입니다.
+public:
+	CMFCBreadFactoryDoc* GetDocument() const;
+
+// 작업입니다.
+public:
+
+// 재정의입니다.
+public:
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+	virtual void OnInitialUpdate(); // 생성 후 처음 호출되었습니다.
+
+// 구현입니다.
+public:
+	virtual ~CMFCBreadFactoryView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 생성된 메시지 맵 함수
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // MFCBreadFactoryView.cpp의 디버그 버전
+inline CMFCBreadFactoryDoc* CMFCBreadFactoryView::GetDocument() const
+   { return reinterpret_cast<CMFCBreadFactoryDoc*>(m_pDocument); }
+#endif
+
