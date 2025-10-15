50e4fb69a603c4ca3c5540e6e35791b7886cedae
Jihwan <wlghks9619@gmail.com>
2025-10-15 10:12:11 +0900

    채팅 프로그램 출력 폼 생성

---
+++ MFCBreadFactory/CChatView.cpp
@@ -0,0 +1,49 @@
+﻿// CChatView.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "MFCBreadFactory.h"
+#include "CChatView.h"
+
+
+// CChatView
+
+IMPLEMENT_DYNCREATE(CChatView, CFormView)
+
+CChatView::CChatView()
+	: CFormView(IDD_CHAT_FORM)
+{
+
+}
+
+CChatView::~CChatView()
+{
+}
+
+void CChatView::DoDataExchange(CDataExchange* pDX)
+{
+	CFormView::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CChatView, CFormView)
+END_MESSAGE_MAP()
+
+
+// CChatView 진단
+
+#ifdef _DEBUG
+void CChatView::AssertValid() const
+{
+	CFormView::AssertValid();
+}
+
+#ifndef _WIN32_WCE
+void CChatView::Dump(CDumpContext& dc) const
+{
+	CFormView::Dump(dc);
+}
+#endif
+#endif //_DEBUG
+
+
+// CChatView 메시지 처리기
--- MFCBreadFactory/Resource.h
+++ MFCBreadFactory/Resource.h
@@ -1,32 +1,35 @@
 ﻿//{{NO_DEPENDENCIES}}
 // Microsoft Visual C++에서 생성한 포함 파일입니다.
-// MFCBreadFactory.rc에서 사용
+// MFCBreadFactory.rc에서 사용되고 있습니다.
 //
-#define IDD_ABOUTBOX				100
-#define IDD_MFCBREADFACTORY_FORM			101
-#define IDR_MAINFRAME				128
-#define IDR_MFCBreadFactoryTYPE				130
-#define IDR_THEME_MENU				200
-#define ID_SET_STYLE				201
-#define ID_VIEW_APPLOOK_WIN_2000	205
-#define ID_VIEW_APPLOOK_OFF_XP		206
-#define ID_VIEW_APPLOOK_WIN_XP		207
-#define ID_VIEW_APPLOOK_OFF_2003	208
-#define ID_VIEW_APPLOOK_VS_2005		209
-#define ID_VIEW_APPLOOK_VS_2008		210
-#define ID_VIEW_APPLOOK_OFF_2007_BLUE	215
-#define ID_VIEW_APPLOOK_OFF_2007_BLACK	216
-#define ID_VIEW_APPLOOK_OFF_2007_SILVER	217
-#define ID_VIEW_APPLOOK_OFF_2007_AQUA	218
-#define ID_VIEW_APPLOOK_WINDOWS_7	219
+#define IDD_ABOUTBOX                    100
+#define IDD_MFCBREADFACTORY_FORM        101
+#define IDR_MAINFRAME                   128
+#define IDR_MFCBreadFactoryTYPE         130
+#define IDR_THEME_MENU                  200
+#define ID_SET_STYLE                    201
+#define ID_VIEW_APPLOOK_WIN_2000        205
+#define ID_VIEW_APPLOOK_OFF_XP          206
+#define ID_VIEW_APPLOOK_WIN_XP          207
+#define ID_VIEW_APPLOOK_OFF_2003        208
+#define ID_VIEW_APPLOOK_VS_2005         209
+#define ID_VIEW_APPLOOK_VS_2008         210
+#define ID_VIEW_APPLOOK_OFF_2007_BLUE   215
+#define ID_VIEW_APPLOOK_OFF_2007_BLACK  216
+#define ID_VIEW_APPLOOK_OFF_2007_SILVER 217
+#define ID_VIEW_APPLOOK_OFF_2007_AQUA   218
+#define ID_VIEW_APPLOOK_WINDOWS_7       219
+#define IDD_CHAT_FORM                   313
+#define IDC_BUTTON1                     1000
+#define IDC_BUTTON2                     1001
 
-// 다음은 새 개체에 사용할 기본값입니다.
-//
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE	310
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		310
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        313
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_SYMED_VALUE           314
 #endif
 #endif
