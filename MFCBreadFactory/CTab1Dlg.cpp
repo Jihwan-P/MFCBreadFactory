f5d9777e4f1501b4f9c957e603d04b9363b498a3
Jihwan <wlghks9619@gmail.com>
2025-10-16 16:24:21 +0900

    카메라 영상 출력 v1

--- MFCBreadFactory/CTab1Dlg.cpp
+++ MFCBreadFactory/CTab1Dlg.cpp
@@ -12,23 +12,175 @@
 IMPLEMENT_DYNAMIC(CTab1Dlg, CDialog)
 
 CTab1Dlg::CTab1Dlg(CWnd* pParent /*=nullptr*/)
-	: CDialog(IDD_TAB1_FORM, pParent)
+	: CDialog(IDD_TAB1_FORM, pParent),
+	m_caps(4),
+	m_frames(4),
+	m_mutexes(4),
+	m_bThreadRunning(false)
 {
 
 }
 
 CTab1Dlg::~CTab1Dlg()
 {
+	m_bThreadRunning = false;
+	for (auto& th : m_threads)
+	{
+		if (th.joinable())
+		{
+			th.join();
+		}
+	}
 }
 
 void CTab1Dlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialog::DoDataExchange(pDX);
+
+	DDX_Control(pDX, IDC_STATIC_CAM1, m_picCam1);
+	DDX_Control(pDX, IDC_STATIC_CAM2, m_picCam2);
+	DDX_Control(pDX, IDC_STATIC_CAM3, m_picCam3);
+	DDX_Control(pDX, IDC_STATIC_CAM4, m_picCam4);
 }
 
 
 BEGIN_MESSAGE_MAP(CTab1Dlg, CDialog)
+	ON_WM_DESTROY()
+	ON_WM_TIMER()
 END_MESSAGE_MAP()
 
-
 // CTab1Dlg 메시지 처리기
+
+BOOL CTab1Dlg::OnInitDialog()
+{
+	CDialog::OnInitDialog();
+
+	m_bThreadRunning = true;
+	for (int i = 0; i < 4; ++i)
+	{
+		// 각 카메라에 대한 스레드 생성
+		m_threads.emplace_back(&CTab1Dlg::CameraThread, this, i);
+	}
+
+	// 100ms 간격으로 화면 갱신 타이머 설정
+	SetTimer(TIMER_UPDATE_CAMERA, 100, NULL);
+
+	return TRUE;
+}
+
+// 각 카메라의 영상 프레임을 독립적으로 받아오는 스레드 함수
+void CTab1Dlg::CameraThread(int index)
+{
+	if (index < 0 || index >= m_cameraUrls.size()) return;
+
+	while (m_bThreadRunning)
+	{
+		try
+		{
+			if (!m_caps[index].isOpened())
+			{
+				if (!m_caps[index].open(m_cameraUrls[index]))
+				{
+					std::this_thread::sleep_for(std::chrono::seconds(5));
+					continue;
+				}
+			}
+
+			cv::Mat tempFrame;
+			if (m_caps[index].read(tempFrame))
+			{
+				std::lock_guard<std::mutex> lock(m_mutexes[index]);
+				m_frames[index] = tempFrame.clone();
+			}
+			else
+			{
+				m_caps[index].release();
+				std::this_thread::sleep_for(std::chrono::seconds(1));
+			}
+		}
+		catch (const cv::Exception& e)
+		{
+			// OpenCV 예외 처리
+			m_caps[index].release();
+			std::this_thread::sleep_for(std::chrono::seconds(5));
+		}
+		std::this_thread::sleep_for(std::chrono::milliseconds(30)); // 약 33fps
+	}
+	if (m_caps[index].isOpened())
+	{
+		m_caps[index].release();
+	}
+}
+
+// 받아온 프레임을 CStatic 컨트롤에 그리는 함수
+void CTab1Dlg::DrawFrame(int index)
+{
+	CStatic* pPic;
+	switch (index)
+	{
+	case 0: pPic = &m_picCam1; break;
+	case 1: pPic = &m_picCam2; break;
+	case 2: pPic = &m_picCam3; break;
+	case 3: pPic = &m_picCam4; break;
+	default: return;
+	}
+
+	if (!pPic->GetSafeHwnd()) return;
+
+	cv::Mat currentFrame;
+	{
+		std::lock_guard<std::mutex> lock(m_mutexes[index]);
+		if (m_frames[index].empty())
+		{
+			return;
+		}
+		currentFrame = m_frames[index].clone();
+	}
+
+	CRect rect;
+	pPic->GetClientRect(&rect);
+
+	// 프레임 크기를 CStatic 컨트롤 크기에 맞게 조절
+	cv::resize(currentFrame, currentFrame, cv::Size(rect.Width(), rect.Height()));
+
+	// Mat을 CImage로 변환하여 화면에 출력
+	CImage cimage;
+	int bpp = currentFrame.channels() * 8;
+	cimage.Create(currentFrame.cols, currentFrame.rows, bpp);
+	if (bpp == 8)
+	{
+		static RGBQUAD gray[256];
+		for (int i = 0; i < 256; i++) gray[i].rgbRed = gray[i].rgbGreen = gray[i].rgbBlue = i;
+		cimage.SetColorTable(0, 256, gray);
+	}
+
+	int pitch = cimage.GetPitch();
+	uchar* pBits = (uchar*)cimage.GetBits();
+
+	memcpy(pBits, currentFrame.data, currentFrame.rows * currentFrame.cols * currentFrame.channels());
+
+	CDC* pDC = pPic->GetDC();
+	cimage.Draw(pDC->GetSafeHdc(), 0, 0);
+	ReleaseDC(pDC);
+	cimage.Destroy();
+}
+
+
+void CTab1Dlg::OnDestroy()
+{
+	CDialog::OnDestroy();
+	KillTimer(TIMER_UPDATE_CAMERA);
+	m_bThreadRunning = false;
+}
+
+void CTab1Dlg::OnTimer(UINT_PTR nIDEvent)
+{
+	if (nIDEvent == TIMER_UPDATE_CAMERA)
+	{
+		for (int i = 0; i < 4; ++i)
+		{
+			DrawFrame(i);
+		}
+	}
+	CDialog::OnTimer(nIDEvent);
+}
--- MFCBreadFactory/CTab1Dlg.h
+++ MFCBreadFactory/CTab1Dlg.h
@@ -1,6 +1,13 @@
 ﻿#pragma once
 #include "afxdialogex.h"
 
+#include <opencv2/opencv.hpp>
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <atomic>
+
+#define TIMER_UPDATE_CAMERA 100
 
 // CTab1View 폼 보기
 
@@ -20,5 +27,32 @@ public:
 protected:
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
 
+	virtual BOOL OnInitDialog(); // 다이얼로그 초기화를 위해 추가
+
+	// OpenCV 및 스레드 관련 멤버 변수
+	std::vector<cv::VideoCapture> m_caps;
+	std::vector<cv::Mat> m_frames;
+	std::vector<std::thread> m_threads;
+	std::atomic<bool> m_bThreadRunning;
+	std::vector<std::mutex> m_mutexes;
+
+	// 카메라 스트리밍 URL
+	const std::vector<std::string> m_cameraUrls = {
+		"http://192.168.0.118:18081", // CAM 1
+		"http://192.168.0.91:18081",  // CAM 2
+		"http://192.168.0.84:18081",  // CAM 3
+		"http://192.168.0.108:18081"  // CAM 4
+	};
+
+	// 카메라 영상을 표시할 CStatic 컨트롤 멤버 변수
+	CStatic m_picCam1, m_picCam2, m_picCam3, m_picCam4;
+
+	void CameraThread(int index);
+	void DrawFrame(int index);
+
 	DECLARE_MESSAGE_MAP()
+
+public:
+	afx_msg void OnDestroy();
+	afx_msg void OnTimer(UINT_PTR nIDEvent);
 };
