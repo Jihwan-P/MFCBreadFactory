2cb6e60fa5ae117a44fd668c29063af5db0a8249
Jihwan <wlghks9619@gmail.com>
2025-10-17 11:48:41 +0900

    Add files via upload

--- MFCBreadFactory/Resource.h
+++ MFCBreadFactory/Resource.h
@@ -1,32 +1,55 @@
 ﻿//{{NO_DEPENDENCIES}}
 // Microsoft Visual C++에서 생성한 포함 파일입니다.
-// MFCBreadFactory.rc에서 사용
+// MFCBreadFactory.rc에서 사용되고 있습니다.
 //
-#define IDD_ABOUTBOX				100
-#define IDD_MFCBREADFACTORY_FORM			101
-#define IDR_MAINFRAME				128
-#define IDR_MFCBreadFactoryTYPE				130
-#define IDR_THEME_MENU				200
-#define ID_SET_STYLE				201
-#define ID_VIEW_APPLOOK_WIN_2000	205
-#define ID_VIEW_APPLOOK_OFF_XP		206
-#define ID_VIEW_APPLOOK_WIN_XP		207
-#define ID_VIEW_APPLOOK_OFF_2003	208
-#define ID_VIEW_APPLOOK_VS_2005		209
-#define ID_VIEW_APPLOOK_VS_2008		210
-#define ID_VIEW_APPLOOK_OFF_2007_BLUE	215
-#define ID_VIEW_APPLOOK_OFF_2007_BLACK	216
-#define ID_VIEW_APPLOOK_OFF_2007_SILVER	217
-#define ID_VIEW_APPLOOK_OFF_2007_AQUA	218
-#define ID_VIEW_APPLOOK_WINDOWS_7	219
+#define IDD_ABOUTBOX                    100
+#define IDD_MFCBREADFACTORY_FORM        101
+#define IDR_MAINFRAME                   128
+#define IDR_MFCBreadFactoryTYPE         130
+#define IDR_THEME_MENU                  200
+#define ID_SET_STYLE                    201
+#define ID_VIEW_APPLOOK_WIN_2000        205
+#define ID_VIEW_APPLOOK_OFF_XP          206
+#define ID_VIEW_APPLOOK_WIN_XP          207
+#define ID_VIEW_APPLOOK_OFF_2003        208
+#define ID_VIEW_APPLOOK_VS_2005         209
+#define ID_VIEW_APPLOOK_VS_2008         210
+#define ID_VIEW_APPLOOK_OFF_2007_BLUE   215
+#define ID_VIEW_APPLOOK_OFF_2007_BLACK  216
+#define ID_VIEW_APPLOOK_OFF_2007_SILVER 217
+#define ID_VIEW_APPLOOK_OFF_2007_AQUA   218
+#define ID_VIEW_APPLOOK_WINDOWS_7       219
+#define IDD_CHAT_FORM                   313
+#define IDD_TAB1_FORM                   318
+#define IDD_TAB2_FORM                   319
+#define IDD_TAB3_FORM                   320
+#define IDD_TAB4_FORM                   321
+#define IDC_BUTTON1                     1000
+#define IDC_BUTTON2                     1001
+#define IDC_TAB                         1003
+#define IDC_TAB1                        1003
+#define IDC_STATIC_CAM1                 1004
+#define IDC_STATIC_CAM2                 1005
+#define IDC_STATIC_CAM3                 1006
+#define IDC_STATIC_CAM4                 1007
+#define IDC_STATIC_GRAPH1               1008
+#define IDC_STATIC_GRAPH2               1009
+#define IDC_STATIC_GRAPH3               1010
+#define IDC_STATIC_GROUP_CAM            1011
+#define IDC_STATIC_GROUP_GRAPH          1012
+#define IDC_LIST_CHAT                   1013
+#define IDC_EDIT_CHAT                   1014
+#define IDC_BUTTON_SEND                 1015
+#define IDC_STATIC_TIME                 1016
 
-// 다음은 새 개체에 사용할 기본값입니다.
-//
+
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE	310
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		310
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        320
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1017
+#define _APS_NEXT_SYMED_VALUE           322
 #endif
 #endif
